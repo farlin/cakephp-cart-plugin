d2369c34cfcda627bc1790b3cfec785e04a7a3c2
farlin <farlin@users.noreply.github.com>
2016-02-25 01:02:04 +1100

    Update for migration files
    corrected typo and removed repeated field name

--- Config/Migration/1394031700_adding_new_fields_and_fixing_existing_fields_for_addresses.php
+++ Config/Migration/1394031700_adding_new_fields_and_fixing_existing_fields_for_addresses.php
@@ -19,25 +19,25 @@ class AddingNewFieldsAndFixingExistingFieldsForAddresses extends CakeMigration {
 		'up' => array(
 			'create_field' => array(
 				'order_addresses' => array(
-					'state' => array('type' => 'string', 'null' => true, 'default' => null, 'lenght' => 2),
+					'state' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 2),
 				),
 				'orders' => array(
-					'shipping_address_id' => array('type' => 'string', 'null' => true, 'default' => null, 'lenght' => 36),
-					'billing_address_id' => array('type' => 'string', 'null' => true, 'default' => null, 'lenght' => 36),
+					'shipping_address_id' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 36),
+					'billing_address_id' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 36),
 					'shipping_rate' => array('float' => 'string', 'null' => false, 'default' => 0.00),
 					'gross' => array('type' => 'float', 'null' => false, 'default' => 0.00),
 				)
 			),
 			'alter_field' => array(
 				'order_addresses' => array(
-					'type' => array('type' => 'string', 'null' => true, 'default' => null, 'lenght' => 16),
+					'type' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 16),
 				)
 			),
 		),
 		'down' => array(
 			'drop_field' => array(
 				'orders' => array(
-					'billing_address_id',
+					'shipping_address_id',
 					'billing_address_id',
 					'shipping_rate',
 					'gross',
@@ -48,7 +48,7 @@ class AddingNewFieldsAndFixingExistingFieldsForAddresses extends CakeMigration {
 			),
 			'alter_field' => array(
 				'order_addresses' => array(
-					'type' => array('type' => 'string', 'null' => true, 'default' => null, 'lenght' => 2),
+					'type' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 2),
 				)
 			)
 		),
